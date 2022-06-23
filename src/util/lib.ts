ee5920c005ee79eb367a8bc0f9577f216614fe90
Leonardo Araujo <mendes.lnr@gmail.com>
2022-06-23 20:28:54 -0300

    Ensuring shuffle array function returns a new array

--- src/util/lib.ts
+++ src/util/lib.ts
@@ -1,8 +1,12 @@
 export const shuffleArray = (array: any[]) => {
   const newArray = [...array];
-  for (let i = newArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  let firstPass = true;
+  while (firstPass || compareArrays(array, newArray)) {
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    firstPass = false;
   }
   return newArray;
 };
@@ -10,7 +14,9 @@ export const shuffleArray = (array: any[]) => {
 export const compareArrays = (a1: any[], a2: any[]) => {
   let i = a1.length;
   while (i--) {
-    if (a1[i] !== a2[i]) return false;
+    if (a1[i] !== a2[i]) {
+      return false;
+    }
   }
   return true;
 };
